f83e3573a090a1e04c6126243913f631902a0afe
Jouni Varis <jouni.varis@gofore.com>
2023-11-14 16:03:24 +0200

    fix useOrientation

--- src/hooks/useOrientation.ts
+++ src/hooks/useOrientation.ts
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 
-const getOrientation = () => window.screen.orientation.type;
+const getOrientation = () =>
+  window?.screen?.orientation?.type ?? 'portrait-primary';
 
 const useScreenOrientation = () => {
   const [orientation, setOrientation] = useState(getOrientation());
